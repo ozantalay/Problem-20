526c09edf39257db70178269c8d9c9a9666d7eb7
ozantalay <ozntalay@hotmail.com>
2024-08-28 20:00:44 +0300

    Final

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { useEffect, useState } from 'react'
+import { data } from "autoprefixer";
+import { useEffect, useState } from "react";
 
 // todos endpoint'inden tüm todo'ları getirin ve todo bileşeninde görüntüleyin
 // API endpoint: https://jsonplaceholder.typicode.com/users/1/todos
@@ -9,30 +10,44 @@ export default function Todos() {
   //     "title": "delectus aut autem",
   //     "completed": false
   // }[]
+  const [todos, setTodos] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users/1/todos")
+      .then((response) => response.json())
+      .then((data) => setTodos(data))
+      .catch((error) =>
+        console.error("veri çekilirken hata meydana geldi", error)
+      );
+  }, []);
 
   return (
-    <div className='flex justify-center flex-col items-center py-8'>
-      <h1 className='text-2xl font-bold pb-4'>Yapılacaklar Listem</h1>
-      <div className='space-y-5'>{/* todos buraya */}</div>
+    <div className="flex justify-center flex-col items-center py-8">
+      <h1 className="text-2xl font-bold pb-4">Yapılacaklar Listem</h1>
+      <div className="space-y-5">
+        {/* todos buraya */}{" "}
+        {todos.map((todo) => (
+          <Todo key={todo.id} title={todo.title} complated={todo.completed} />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-function Todo() {
+function Todo({ completed, title }) {
   return (
-    <div className='relative flex items-start'>
-      <div className='flex h-6 items-center'>
+    <div className="relative flex items-start">
+      <div className="flex h-6 items-center">
         <input
-          id='completed'
-          name='completed'
-          type='checkbox'
-          defaultChecked={false}
-          className='h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600'
+          id="completed"
+          name="completed"
+          type="checkbox"
+          defaultChecked={completed}
+          className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
         />
       </div>
-      <div className='ml-3 text-sm leading-6'>
-        <div className='font-medium text-gray-900'>Başlık</div>
+      <div className="ml-3 text-sm leading-6">
+        <div className="font-medium text-gray-900">{title}</div>
       </div>
     </div>
-  )
+  );
 }
